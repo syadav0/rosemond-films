2222a5fec16f7293674d3ef0516ccd056bfc1dfb
syadav0 <sushantryadav1@gmail.com>
2023-08-04 16:42:02 -0400

    bruh 2

--- src/app/api/send/route.tsx
+++ src/app/api/send/route.tsx
@@ -3,7 +3,7 @@ import sg from "@sendgrid/mail";
 export async function POST(req: Request, res: Response) {
   sg.setApiKey(process.env.SENDGRID_API_KEY!);
   const to_e = process.env.TO_EMAIL;
-  const from_e = process.env.FROM_EMAIL;
+  const from_e = process.env.FROM_EMAIL!;
   
   try {
     console.log("Sending email...");
@@ -11,13 +11,13 @@ export async function POST(req: Request, res: Response) {
     const { name, email, msg } = data;
     
     console.log(name, email, msg, to_e, from_e);
-    // await sg.send({
-    //   to: to_e,
-    //   from: from_e,
-    //   subject: `${name} has sent a message from your site!`,
-    //   html: `<h4> ${name} has sent a message, their email is ${email}.</h4> 
-    //   <br> Message: ${msg}`,
-    // });
+    await sg.send({
+      to: to_e,
+      from: from_e,
+      subject: `${name} has sent a message from your site!`,
+      html: `<h4> ${name} has sent a message, their email is ${email}.</h4> 
+      <br> Message: ${msg}`,
+    });
   } catch (err) {
     console.log("Failed");
     console.log(err);
