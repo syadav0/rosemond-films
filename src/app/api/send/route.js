6d76f91ec4af61339f3b856bff082cf5ab3d46d5
syadav0 <sushantryadav1@gmail.com>
2023-08-04 15:52:42 -0400

    asdsa

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -4,10 +4,11 @@ export async function POST(req, res) {
   sg.setApiKey(process.env.SENDGRID_API_KEY);
   const to_e = process.env.TO_EMAIL;
   const from_e = process.env.FROM_EMAIL;
-  const b = await req.json();
 
+  const b = await req.json();
   try {
     console.log("Sending email...");
+    console.log(b.name, b.email, b.msg);
     await sg.send({
       to: to_e,
       from: from_e,
